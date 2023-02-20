1b33417310ea6109a96312714abc6a7ad2e2b36e
Mimo <mimoriam420@gmail.com>
2023-02-20 16:31:15 +0500

    Commit #1 - Project initialization

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+// npm i class-validator class-transformer @nestjs/config @nestjs/throttler @nestjs/typeorm typeorm pg cookie-parser bcrypt cookie uuid
+// npm i -D @types/cookie-parser @types/bcrypt @types/cookie @types/uuid
+
+// # Sessions:
+// npm i passport passport-local @nestjs/passport express-session connect-typeorm
+// npm i -D @types/passport @types/passport-local @types/express-session
+async function bootstrap() {
+  const { PORT } = process.env;
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  try {
+    await app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+bootstrap().then();
