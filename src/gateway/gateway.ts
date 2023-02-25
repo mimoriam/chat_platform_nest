84c399b5cc3535b06196d7598030b1a96bd713cd
Mimo <mimoriam420@gmail.com>
2023-02-25 21:54:17 +0500

    Commit #38 - Emit onGroupOwnerUpdate to user when they are not in the room + Emit onGroupParticipantLeft when a group user leaves

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -249,6 +249,23 @@ export class MessagingGateway
   @OnEvent('group.owner.update')
   handleGroupOwnerUpdate(payload: Group) {
     const ROOM_NAME = `group-${payload.id}`;
+    const newOwnerSocket = this.sessions.getUserSocket(payload.owner.id);
+
+    const { rooms } = this.server.sockets.adapter;
+
+    const socketsInRoom = rooms.get(ROOM_NAME);
+
+    // Check if the new owner is in the group (room)
     this.server.to(ROOM_NAME).emit('onGroupOwnerUpdate', payload);
+    if (newOwnerSocket && !socketsInRoom.has(newOwnerSocket.id)) {
+      console.log('The new owner is not in the room...');
+      newOwnerSocket.emit('onGroupOwnerUpdate', payload);
+    }
+  }
+
+  @OnEvent('group.user.leave')
+  handleGroupUserLeave(payload) {
+    const ROOM_NAME = `group-${payload.group.id}`;
+    this.server.to(ROOM_NAME).emit('onGroupParticipantLeft', payload);
   }
 }
